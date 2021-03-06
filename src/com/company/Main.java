44cd5c7cc4e3f954f0ea8dc21a1bfe246058574d
ChiyoAn <obertbd@gmail.com>
2021-03-06 16:24:30 +0200

    ghgggg

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://trello.com/");
+        Thread.sleep(2000);
+        System.out.println("Title " + driver.getTitle());
+        driver.quit();
+
+    }
+}
---
+++ src/tests/LoginTests.java
@@ -0,0 +1,125 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTests {
+   WebDriver driver = new ChromeDriver();
+
+   @BeforeMethod
+   public void initTest() throws InterruptedException {
+
+       driver.get("https://trello.com/");
+       Thread.sleep(2000);
+
+
+
+   }
+
+   @Test
+   public void applicationTest(){
+       System.out.println("Title " + driver.getTitle());
+
+
+
+   }
+
+
+
+
+
+    @Test
+    public void loginNegativeLoginIncorrect() throws InterruptedException {
+
+        WebElement loginIncon = driver.findElement(By.xpath(" //a[contains(text(),'Войти')]"));
+        loginIncon.click();
+        Thread.sleep(5000);
+        WebElement loginField = driver.findElement(By.xpath("//input[@id='user']"));
+        fillField(loginField, "123");
+        Thread.sleep(2000);
+        WebElement passwordField = driver.findElement(By.xpath("//input[@id='password']"));
+        fillField(passwordField, "qqq");
+        Thread.sleep(3000);
+        driver.findElement(By.id("login")).click();
+
+        System.out.println("qqqq" + driver.findElements(By.className("error-message")).get(1).getText() );
+        Thread.sleep(2000);
+
+
+    }
+
+
+    @Test
+    public void loginPositiveTest() throws InterruptedException {
+
+        WebElement loginIncon = driver.findElement(By.xpath(" //a[contains(text(),'Войти')]"));
+        loginIncon.click();
+        Thread.sleep(5000);
+        WebElement loginField = driver.findElement(By.xpath("//input[@id='user']"));
+        fillField(loginField, "obertbd@gmail.com");
+        Thread.sleep(2000);
+        WebElement passwordField = driver.findElement(By.xpath("//input[@id='password']"));
+        fillField(passwordField, "KbyfBydthc94");
+        Thread.sleep(3000);
+        driver.findElement(By.id("login")).click();
+        Thread.sleep(10000);
+        WebElement boardsButton = driver.findElement(By.className("MEu8ZECLGMLeab"));
+        System.out.println("boardsButton: " + boardsButton.getText());
+        Thread.sleep(2000);
+
+
+
+    }
+    @Test
+    public void loginWithNotCorrectPassword() throws InterruptedException {
+        WebElement loginIncon = driver.findElement(By.xpath(" //a[contains(text(),'Войти')]"));
+        loginIncon.click();
+        Thread.sleep(5000);
+        WebElement loginField = driver.findElement(By.xpath("//input[@id='user']"));
+        fillField(loginField, "obertbd@gmail.com");
+        Thread.sleep(2000);
+        WebElement passwordField = driver.findElement(By.xpath("//input[@id='password']"));
+        fillField(passwordField, "qqqwerertfd");
+        Thread.sleep(3000);
+        driver.findElement(By.id("login")).click();
+        Thread.sleep(2000);
+
+
+        System.out.println("Error: " + driver.findElement(By.className("forgotLink")).getText());
+        System.out.println("Error2 " + driver.findElement(By.xpath("//body/div[1]/section[1]/div[1]/div[1]/div[3]/p[1]")).getText() );
+
+        Thread.sleep(2000);
+
+
+
+
+
+    }
+
+
+
+
+
+    @AfterMethod
+    public void tearDown(){
+       driver.quit();
+
+
+
+    }
+
+    public void fillField(WebElement element, String value) {
+        element.clear();
+        element.click();
+        element.sendKeys(value);
+    }
+
+
+
+
+}
